8727e4cb7827bac9143ea06c4e3931c70428897b
Alvaro Capaceta <ingcapadev@gmail.com>
2023-11-01 10:43:02 -0700

    feat: Question card component and handle selected answer

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,4 +1,11 @@
-import { Card, Typography } from '@mui/material';
+import {
+  Card,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Typography,
+} from '@mui/material';
 import { useQuestionsStore } from './store/questions';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import SyntaxTheme from 'react-syntax-highlighter/dist/esm/styles/hljs/gradient-dark';
@@ -9,6 +16,21 @@ type QuestionProp = {
 };
 
 const Question: React.FC<QuestionProp> = ({ info }) => {
+  const selectAnswer = useQuestionsStore((state) => state.selectAnswer);
+  const createHandleClick = (answerIndex: number) => () => {
+    selectAnswer(info.id, answerIndex);
+  };
+
+  const getBackgroundColor = (answerIndex: number) => {
+    const { userSelectedAnswer, correctAnswer } = info;
+    if (userSelectedAnswer == null) return 'transparent';
+    if (answerIndex !== correctAnswer && answerIndex !== userSelectedAnswer)
+      return 'transparent';
+    if (userSelectedAnswer !== null) {
+      if (answerIndex === correctAnswer) return 'green';
+      if (answerIndex === userSelectedAnswer) return 'red';
+    }
+  };
   return (
     <Card
       variant='outlined'
@@ -16,11 +38,26 @@ const Question: React.FC<QuestionProp> = ({ info }) => {
         textAlign: 'left',
         padding: 2,
         bgcolor: '#222',
+        marginTop: 4,
       }}>
       <Typography variant='h5'>{info.question}</Typography>
       <SyntaxHighlighter language='javascript' style={SyntaxTheme}>
         {info.code}
       </SyntaxHighlighter>
+      <List sx={{ bgcolor: '#333' }} disablePadding>
+        {info.answers.map((answer, index) => (
+          <ListItem key={index} disablePadding divider>
+            <ListItemButton
+              disabled={info.userSelectedAnswer != undefined}
+              onClick={createHandleClick(index)}
+              sx={{
+                backgroundColor: getBackgroundColor(index),
+              }}>
+              <ListItemText primary={answer} sx={{ textAlign: 'center' }} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
     </Card>
   );
 };
--- src/store/questions.ts
+++ src/store/questions.ts
@@ -1,13 +1,15 @@
 import { type Question } from '../types';
 import { create } from 'zustand';
+import confetti from 'canvas-confetti';
 
 type State = {
   questions: Question[];
   currentQuestion: number;
-  fetchQuestions: (limit: number) => void;
+  fetchQuestions: (limit: number) => Promise<void>;
+  selectAnswer: (questionID: number, answerIndex: number) => void;
 };
 
-const initialState: Omit<State, 'fetchQuestions'> = {
+const initialState: Omit<State, 'fetchQuestions' | 'selectAnswer'> = {
   questions: [],
   currentQuestion: 0,
 };
@@ -21,6 +23,32 @@ export const useQuestionsStore = create<State>((set, get) => {
       const json = await res.json();
       const questions = json.sort(() => Math.random() - 0.5).slice(0, limit);
       set({ questions });
+    },
+
+    selectAnswer: (questionID: number, answerIndex: number) => {
+      // use StructureClone to avoid mutating the state
+      const { questions } = get();
+      const newQuestions = structuredClone(questions);
+
+      // find the question and check if the answer is correct
+      const questionIndex = newQuestions.findIndex(
+        (question) => question.id === questionID
+      );
+      const questionInfo = newQuestions[questionIndex];
+      const isCorrectUserAnswer = questionInfo.correctAnswer === answerIndex;
+
+      if (isCorrectUserAnswer) {
+        confetti();
+      }
+
+      // update the state
+      newQuestions[questionIndex] = {
+        ...questionInfo,
+        isCorrectUserAnswer,
+        userSelectedAnswer: answerIndex,
+      };
+
+      set({ questions: newQuestions });
     },
   };
 });
