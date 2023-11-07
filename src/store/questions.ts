adb0fc6bcfc33790709398da9a05cb02a751bfdb
Alvaro Capaceta <ingcapadev@gmail.com>
2023-11-06 17:20:07 -0700

    feat: Zustand persistance data with local storage

--- src/store/questions.ts
+++ src/store/questions.ts
@@ -1,6 +1,7 @@
 import { type Question } from '../types';
 import { create } from 'zustand';
 import confetti from 'canvas-confetti';
+import { persist } from 'zustand/middleware';
 
 type State = {
   questions: Question[];
@@ -17,61 +18,69 @@ const initialState: Pick<State, 'questions' | 'currentQuestion'> = {
   currentQuestion: 0,
 };
 
-export const useQuestionsStore = create<State>((set, get) => {
-  return {
-    ...initialState,
+export const useQuestionsStore = create(
+  persist<State>(
+    (set, get) => {
+      return {
+        ...initialState,
 
-    fetchQuestions: async (limit: number) => {
-      const res = await fetch('http://localhost:5173/data.json');
-      const json = await res.json();
-      const questions = json.sort(() => Math.random() - 0.5).slice(0, limit);
-      set({ questions });
-    },
+        fetchQuestions: async (limit: number) => {
+          const res = await fetch('http://localhost:5173/data.json');
+          const json = await res.json();
+          const questions = json
+            .sort(() => Math.random() - 0.5)
+            .slice(0, limit);
+          set({ questions });
+        },
 
-    selectAnswer: (questionID: number, answerIndex: number) => {
-      // use StructureClone to avoid mutating the state
-      const { questions } = get();
-      const newQuestions = structuredClone(questions);
+        selectAnswer: (questionID: number, answerIndex: number) => {
+          // use StructureClone to avoid mutating the state
+          const { questions } = get();
+          const newQuestions = structuredClone(questions);
 
-      // find the question and check if the answer is correct
-      const questionIndex = newQuestions.findIndex(
-        (question) => question.id === questionID
-      );
-      const questionInfo = newQuestions[questionIndex];
-      const isCorrectUserAnswer = questionInfo.correctAnswer === answerIndex;
+          // find the question and check if the answer is correct
+          const questionIndex = newQuestions.findIndex(
+            (question) => question.id === questionID
+          );
+          const questionInfo = newQuestions[questionIndex];
+          const isCorrectUserAnswer =
+            questionInfo.correctAnswer === answerIndex;
 
-      if (isCorrectUserAnswer) {
-        confetti();
-      }
+          if (isCorrectUserAnswer) {
+            confetti();
+          }
 
-      // update the state
-      newQuestions[questionIndex] = {
-        ...questionInfo,
-        isCorrectUserAnswer,
-        userSelectedAnswer: answerIndex,
+          // update the state
+          newQuestions[questionIndex] = {
+            ...questionInfo,
+            isCorrectUserAnswer,
+            userSelectedAnswer: answerIndex,
+          };
+
+          set({ questions: newQuestions });
+        },
+
+        goNextQuestion: () => {
+          const { currentQuestion, questions } = get();
+          const nextQuestion = currentQuestion + 1;
+          if (nextQuestion < questions.length) {
+            set({ currentQuestion: nextQuestion });
+          }
+        },
+
+        goPreviousQuestion: () => {
+          const { currentQuestion } = get();
+          const previousQuestion = currentQuestion - 1;
+          if (previousQuestion >= 0) {
+            set({ currentQuestion: previousQuestion });
+          }
+        },
+
+        reset: () => {
+          set({ ...initialState });
+        },
       };
-
-      set({ questions: newQuestions });
     },
-
-    goNextQuestion: () => {
-      const { currentQuestion, questions } = get();
-      const nextQuestion = currentQuestion + 1;
-      if (nextQuestion < questions.length) {
-        set({ currentQuestion: nextQuestion });
-      }
-    },
-
-    goPreviousQuestion: () => {
-      const { currentQuestion } = get();
-      const previousQuestion = currentQuestion - 1;
-      if (previousQuestion >= 0) {
-        set({ currentQuestion: previousQuestion });
-      }
-    },
-
-    reset: () => {
-      set({ ...initialState });
-    },
-  };
-});
+    { name: 'questions' }
+  )
+);
